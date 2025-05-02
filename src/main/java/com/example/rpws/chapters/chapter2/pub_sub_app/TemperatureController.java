869e2dc195b69a77ccba581ec73dbbc357a28520
victor alvarez <victorfelipealvarezrojas@hotmail.com>
2025-05-01 21:39:50 -0400

    se agrega beans para emitir flujos de valores random de forma automatica

--- src/main/java/com/example/rpws/chapters/chapter2/pub_sub_app/TemperatureController.java
+++ src/main/java/com/example/rpws/chapters/chapter2/pub_sub_app/TemperatureController.java
@@ -1,4 +1,47 @@
 package com.example.rpws.chapters.chapter2.pub_sub_app;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.event.EventListener;
+import org.springframework.http.MediaType;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+@RestController
 public class TemperatureController {
+
+    private final Set<SseEmitter> clients = new CopyOnWriteArraySet<>();
+
+
+    @GetMapping("/temperature-stream")
+    public SseEmitter events(HttpServletRequest  request) {
+        SseEmitter emitter = new SseEmitter();
+        clients.add(emitter);
+
+        // Remove emitter from clients on error or disconnect
+        emitter.onTimeout(() -> clients.remove(emitter));
+        emitter.onCompletion(() -> clients.remove(emitter));
+        return emitter;
+    }
+
+
+    @Async
+    @EventListener
+    public void handleMessage(Temperature temperature) {
+        List<SseEmitter> deadEmitters = new ArrayList<>();
+
+        clients.forEach(emitter -> {
+            try {
+                emitter.send(temperature, MediaType.APPLICATION_JSON);
+            } catch (Exception e) {
+                deadEmitters.add(emitter);
+            }
+        });
+    }
 }
