ebcee4aaf1b57b38a72b7018d55ce1e9be8643f9
victor alvarez <victorfelipealvarezrojas@hotmail.com>
2025-05-01 20:03:29 -0400

    se agrega beans para emitir flujos de valores random de forma automatica

--- src/main/java/com/example/rpws/chapters/chapter2/pub_sub_app/Temperature.java
+++ src/main/java/com/example/rpws/chapters/chapter2/pub_sub_app/Temperature.java
@@ -1,4 +1,20 @@
 package com.example.rpws.chapters.chapter2.pub_sub_app;
 
 public class Temperature {
+    private final double value;
+
+    public Temperature(double value) {
+        this.value = value;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "Temperature{" +
+                "value=" + value +
+                '}';
+    }
 }
